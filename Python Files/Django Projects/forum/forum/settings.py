8bddb235112d630bf75a962711597a69bd70fcdc
groofinn <LennyFace228@gmail.com>
2021-02-22 20:17:20 +0300

    added simple css

--- Python Files/Django Projects/forum/forum/settings.py
+++ Python Files/Django Projects/forum/forum/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'forum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,3 +109,7 @@ STATIC_URL = '/static/'
 
 # Added by me
 LOGIN_REDIRECT_URL = '/accounts/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
